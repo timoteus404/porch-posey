f9b6f08b0d08ae7db02e75444944529e7b267410
timoteus404 <tyahyayev@yahoo.com>
2021-01-21 07:30:14 +0500

    Checking the status of git

--- src/main/java/org/example/HibernateApplication.java
+++ src/main/java/org/example/HibernateApplication.java
@@ -11,7 +11,6 @@ public class HibernateApplication {
 
         SalesRepRepository salesRep = new SalesRepRepoImplementation();
 
-
         SalesReps one = new SalesReps("James", new Regions(1L));
         salesRep.save(one);
 
